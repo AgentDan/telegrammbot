d458208bbc4d41b8978ae5522f54e93ad210713c
AgentDan <bion-1@mail.ru>
2025-02-04 22:16:21 +0100

    com5

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const TelegramBot = require('node-telegram-bot-api');
+const async_hooks = require("node:async_hooks");
+const webAppUrl = 'https://ya.ru'
 
 const dotenv = require("dotenv").config()
 
@@ -9,10 +11,27 @@ const token = process.env.TOKEN
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
+    const text = msg.text
 
-    bot.sendMessage(chatId, 'I LOVE YOU BABY !!!');
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполни форму', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+    await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+        reply_markup: {
+            inline_keyboard: [
+                [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+            ]
+        }
+    })
+
 });
 
-app.listen(PORT, ()=> console.log(`Server started on port : ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port : ${PORT}`))
