4430d233751cc76a399f304df77f4555289131eb
AgentDan <bion-1@mail.ru>
2025-02-04 20:00:32 +0100

    com4

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const app = express()
+const TelegramBot = require('node-telegram-bot-api');
+
 const dotenv = require("dotenv").config()
 
 const PORT = process.env.PORT
+const token = process.env.TOKEN
 
-app.get("/", (req, res)=>
-    res.send('HELLO WORLD!!!!')
-)
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'I LOVE YOU BABY !!!');
+});
 
 app.listen(PORT, ()=> console.log(`Server started on port : ${PORT}`))
