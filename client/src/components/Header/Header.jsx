a3cee04398b97f7351b909c2607c5ca05d1164f8
AgentDan <bion-1@mail.ru>
2025-02-10 19:20:25 +0100

    commit com2

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,7 +8,10 @@ const Header = () => {
 
     return (
         <div className={'header'}>
-            <div className="cursor-pointer hover:bg-gray-300 w-[100px] text-center ml-6 rounded-[5px] bg-[#30A3E6]">Закрыть</div>
+            <div
+                className="cursor-pointer hover:bg-gray-300 w-[100px] text-center ml-6 rounded-[5px] bg-[##2AABEE]"
+                onClick={onClose}
+            >Закрыть</div>
             {/*<Button>Закрыть</Button>*/}
             <span className="ml-0">
                 {user?.username}
--- client/src/components/ProductListTwo/ProductListTwo.jsx
+++ client/src/components/ProductListTwo/ProductListTwo.jsx
@@ -120,7 +120,7 @@ const ProductListTwo = () => {
             {own ?
                 (
                     <>
-                        <div className=" cursor-pointer hover:bg-gray-300 w-[100px] text-center ml-6 rounded-[5px] bg-[#30A3E6]"
+                        <div className=" cursor-pointer hover:bg-gray-300 w-[100px] text-center ml-6 rounded-[5px] bg-[#2AABEE]"
                              onClick={() => setOwn("")}
                         >
                             Menu!!!
