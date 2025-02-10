adc4d57be4d592a3e8a626b422443c1b57d0ab79
AgentDan <bion-1@mail.ru>
2025-02-10 17:46:39 +0100

    commit

--- client/src/components/ProductListTwo/ProductListTwo.jsx
+++ client/src/components/ProductListTwo/ProductListTwo.jsx
@@ -6,16 +6,51 @@ import {useCallback, useEffect} from "react";
 import {v4 as uuidv4} from "uuid";
 
 const products = [
-    {id: uuidv4(), own: "суп", img: "sup1.jpeg", title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: uuidv4(), own: "суп", img: "sup1.jpeg", title: 'рассол', price: 5000, description: 'Синего цвета, прямые'},
     {id: uuidv4(), own: "суп", img: "sup2.jpg", title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
     {id: uuidv4(), own: "суп", img: "sup3.jpg", title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
     {id: uuidv4(), own: "суп", img: "sup4.jpg", title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: uuidv4(), own: "лапша", img: "lapsha1.jpg", title: 'Ремень', price: 1322, description: 'Зеленого цвета, теплая'},
+    {
+        id: uuidv4(),
+        own: "лапша",
+        img: "lapsha1.jpg",
+        title: 'Ремень',
+        price: 1322,
+        description: 'Зеленого цвета, теплая'
+    },
     {id: uuidv4(), own: "лапша", img: "lapsha2.jpg", title: 'Пряжка', price: 22, description: 'Зеленого цвета, теплая'},
-    {id: uuidv4(), own: "лапша", img: "lapsha3.jpg", title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: uuidv4(), own: "лапша", img: "lapsha4.jpeg", title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: uuidv4(), own: "картошка", img: "potato1.jpg", title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: uuidv4(), own: "картошка", img: "potato2.jpg", title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {
+        id: uuidv4(),
+        own: "лапша",
+        img: "lapsha3.jpg",
+        title: 'Джинсы 3',
+        price: 5000,
+        description: 'Синего цвета, прямые'
+    },
+    {
+        id: uuidv4(),
+        own: "лапша",
+        img: "lapsha4.jpeg",
+        title: 'Куртка 7',
+        price: 600,
+        description: 'Зеленого цвета, теплая'
+    },
+    {
+        id: uuidv4(),
+        own: "картошка",
+        img: "potato1.jpg",
+        title: 'Джинсы 4',
+        price: 5500,
+        description: 'Синего цвета, прямые'
+    },
+    {
+        id: uuidv4(),
+        own: "картошка",
+        img: "potato2.jpg",
+        title: 'Куртка 5',
+        price: 12000,
+        description: 'Зеленого цвета, теплая'
+    },
 ]
 
 const categories = ["суп", "лапша", "картошка"]
@@ -62,7 +97,7 @@ const ProductListTwo = () => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
 
-        if(alreadyAdded) {
+        if (alreadyAdded) {
             newItems = addedItems.filter(item => item.id !== product.id);
         } else {
             newItems = [...addedItems, product];
@@ -70,7 +105,7 @@ const ProductListTwo = () => {
 
         setAddedItems(newItems)
 
-        if(newItems.length === 0) {
+        if (newItems.length === 0) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
@@ -85,8 +120,13 @@ const ProductListTwo = () => {
             {own ?
                 (
                     <>
-                        <div className="cursor-pointer hover:bg-gray-300" onClick={() => setOwn("")}>Menu!!!</div>
-                        <div className={'list'}>
+                        <div className="cursor-pointer hover:bg-gray-300 border-2 px-2 "
+                             onClick={() => setOwn("")}
+                        >
+                            Menu!!!
+                        </div>
+                        <div
+                            className={'list'}>
                             {products.map(item => (
                                 item.own === own &&
                                 <ProductItem
@@ -108,7 +148,7 @@ const ProductListTwo = () => {
                                 <div
                                     key={index}
                                     className={'item cursor-pointer hover:bg-gray-300 border-2 px-2 '}
-                                    onClick={()=>onClickCat(itemcat)}
+                                    onClick={() => onClickCat(itemcat)}
                                 >{itemcat}</div>
                             ))
                         }
