2300a78cade80b3f27ab0f760fb1cd66d47aa344
AgentDan <bion-1@mail.ru>
2025-02-15 11:08:58 +0100

    comiit

--- client/src/components/State/state.js
+++ client/src/components/State/state.js
@@ -1,7 +1,7 @@
 import {v4 as uuidv4} from "uuid";
 
 export const state = [
-
+    //суп
     {
         id: uuidv4(),
         own: "суп",
@@ -13,6 +13,8 @@ export const state = [
     {id: uuidv4(), own: "суп", img: "sup2.jpg", title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
     {id: uuidv4(), own: "суп", img: "sup3.jpg", title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
     {id: uuidv4(), own: "суп", img: "sup4.jpg", title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+
+    //лапша
     {
         id: uuidv4(),
         own: "лапша",
@@ -38,6 +40,8 @@ export const state = [
         price: 600,
         description: 'Зеленого цвета, теплая'
     },
+
+    //картошка
     {
         id: uuidv4(),
         own: "картошка",
@@ -55,4 +59,87 @@ export const state = [
         description: 'Зеленого цвета, теплая'
     },
 
+    //горячие блюда
+    {
+        id: uuidv4(),
+        own: "горячие блюда",
+        img: "meat1.jpg",
+        title: 'Куриные тефтели',
+        price: 400,
+        description: 'Hежные мясные шарики из куриного фарша'
+    },
+    {
+        id: uuidv4(),
+        own: "горячие блюда",
+        img: "pasta.jpg",
+        title: 'Паста Аматричана',
+        price: 650,
+        description: 'Классическое итальянское блюдо из макарон'
+    },
+    {
+        id: uuidv4(),
+        own: "горячие блюда",
+        img: "pjure.jpg",
+        title: 'Картофельное пюре',
+        price: 290,
+        description: 'Классический рецепт картофельного пюре — с молоком и сливочным маслом.'
+    },
+    {
+        id: uuidv4(),
+        own: "горячие блюда",
+        img: "kotleta.jpg",
+        title: 'Котлета из индейки',
+        price: 330,
+        description: 'Сочные котлеты из индейки с ароматным маслом.'
+    },
+    {
+        id: uuidv4(),
+        own: "горячие блюда",
+        img: "semga.jpeg",
+        title: 'Семга запеченая',
+        price: 950,
+        description: 'Сёмга, запечённая с луком и картофелем, постный стол.'
+    },
+    {
+        id: uuidv4(),
+        own: "горячие блюда",
+        img: "file.jpeg",
+        title: 'Филе индейки',
+        price: 450,
+        description: 'Филе индейки, запеченное в духовке'
+    },
+    {
+        id: uuidv4(),
+        own: "горячие блюда",
+        img: "kuskus.jpg",
+        title: 'Кус-кус',
+        price: 300,
+        description: 'кус-кус с овощами'
+    },
+    {
+        id: uuidv4(),
+        own: "горячие блюда",
+        img: "makaroni.jpeg",
+        title: 'Макароны с сыром',
+        price: 450,
+        description: 'Макароны с сыром: простой, но вкусный рецепт.'
+    },
+
+    //салат
+    {
+        id: uuidv4(),
+        own: "салат",
+        img: "salat.jpg",
+        title: 'Салат овощной',
+        price: 250,
+        description: 'Овощной салат со сметаной и зеленью'
+    },
+    {
+        id: uuidv4(),
+        own: "салат",
+        img: "cezar.jpeg",
+        title: 'Цезарь с курицей',
+        price: 800,
+        description: 'Салат Цезарь классический с курицей'
+    },
 ]
